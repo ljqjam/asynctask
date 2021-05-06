20ddb169f340031b91a91dda017a0d2330191aee
01388029 <junqianglin@sfmail.sf-express.com>
2021-05-06 12:48:12 +0800

    update readme

--- src/main/java/com/ljq/asynctask/work/TaskCoordinator.java
+++ src/main/java/com/ljq/asynctask/work/TaskCoordinator.java
@@ -30,11 +30,11 @@ public class TaskCoordinator {
         this.asyncThreadPoolTaskExecutor = asyncThreadPoolTaskExecutor;
         this.workExecutor = new AsyncTaskExecutor();
 
-        //一个SingleThreadPool复杂监听（epoll）
+        //一个SingleThreadPool复杂监听
         LinkedBlockingQueue<Runnable> listenQueue = new LinkedBlockingQueue<Runnable>(1);
         ThreadFactory listenThreadFactory = (new NamedThreadFactory("task-listener"));
         ExecutorService listenService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.SECONDS, listenQueue, listenThreadFactory);
-        //死循环监听，分发
+        //死循环监听，分发（尝试，类似epoll的一个模型）
         listenService.submit(this::listen);
     }
 
